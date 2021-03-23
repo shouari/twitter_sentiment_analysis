c6565e08d46e6697af9dd01c375a01f1caefe896
Salim Houari <salim.houari@gmail.com>
2021-03-23 11:04:27 +0300

    Extract tweets

--- main.py
+++ main.py
@@ -1,68 +1,36 @@
-from textblob import TextBlob
 from twitter_authenticate import authenticate
-import sys
+import csv
 import tweepy
-# import matplotlib.pyplot as plt
-# import pandas as pd
-# import numpy as np
-# import os
-# import nltk
-# import pycountry
-# import re
-# import string
-# from wordcloud import WordCloud, STOPWORDS
-# from PIL import Image
-# from nltk.sentiment.vader import SentimentIntensityAnalyzer
-# from langdetect import detect
-# from nltk.stem import SnowballStemmer
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-# from sklearn.feature_extraction.text import CountVectorizer
-
-def percentage(part,whole):
- return 100 * float(part)/float(whole)
+import time
+import pandas as pd
+import GetOldTweets3 as got
 
 api = authenticate()
 
-keyword = input("Enter keyword or hashtag to search:")
-noOfTweet = input("Enter how many tweets to analyse:")
+text_querry = "GoRafeeq"
+retweet_filter='exclude:retweets'
+querry = text_querry
+count=11000
+try:
 
-tweets = tweepy.Cursor(api.search, q=keyword).items(noOfTweet)
-print(tweets)
-positive = 0
-negative = 0
-neutral = 0
-polarity = 0
-tweet_list = []
-neutral_list = []
-negative_list = []
-positive_list = []
+    tweets = tweepy.Cursor(api.search, q=querry, tweet_mode='extended', since="2021-01-01").items(count)
+    #         if tweet.user.screen_name != 'GoRafeeq' and tweet.user.screen_name!= 'khalid1981s':
+    tweets_list = [[tweet.created_at, tweet.user.screen_name, tweet.full_text] for tweet in tweets]
+    tweets_df = pd.DataFrame(tweets_list)
+    tweets_df.to_csv('tweets_extracted.csv')
+except BaseException as e:
+    print('failed on_status,',str(e))
+    time.sleep(3)
 
-for tweet in tweets:
-
-    # print(tweet.text)
-    tweet_list.append(tweet.text)
-    analysis = TextBlob(tweet.text)
-    score = SentimentIntensityAnalyzer().polarity_scores(tweet.text)
-    neg = score['neg']
-    neu = score['neu']
-    pos = score['pos']
-    comp = score[ 'compound']
-    polarity += analysis.sentiment.polarity
-
-    if neg > pos:
-        negative_list.append(tweet.text)
-        negative += 1
-    elif pos > neg:
-        positive_list.append(tweet.text)
-        positive += 1
-
-    elif pos == neg:
-        neutral_list.append(tweet.text)
-        neutral += 1
-        positive = percentage(positive, noOfTweet)
-        negative = percentage(negative, noOfTweet)
-        neutral = percentage(neutral, noOfTweet)
-        polarity = percentage(polarity, noOfTweet)
-        positive = format(positive, '.1f')
-        negative = format(negative, '.1f')
-        neutral = format(neutral, '.1f')
+# print
+#
+# with open('tweets_extracted.csv','w',newline='', encoding='utf8') as file:
+#     fieldnames = ["tweet_created", 'user_name', 'tweet']
+#     csvWriter = csv.DictWriter(file, fieldnames=fieldnames)
+#     csvWriter.writeheader()
+#
+#     results = api.search(q=querry, tweet_mode='extended', count=count)
+#     for tweet in results:
+#         if tweet.user.screen_name != 'GoRafeeq' and tweet.user.screen_name!= 'khalid1981s':
+#             # print(tweet.user.screen_name, " Tweeted", tweet.text)
+#             csvWriter.writerow({"tweet_created":tweet.created_at,'user_name': tweet.user.screen_name, 'tweet':tweet.full_text})
